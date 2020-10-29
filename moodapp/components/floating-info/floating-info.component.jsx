8b752d0ae61a9455189dbfc4941d9a73b017a61e
blah <blah@blah.com>
2020-10-29 16:34:30 +1300

    end of week commit

--- moodapp/components/floating-info/floating-info.component.jsx
+++ moodapp/components/floating-info/floating-info.component.jsx
@@ -1,78 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Parallax } from "react-parallax";
 
 import {
   SectionContainer,
-  Container,
+  Container
 } from "../floating-info/floating.info.styles";
 import { CloseInfoBtn } from "../custom-button/custom-button.styles";
 import SectionBox from "../section-box/section-box.component";
 
 //TODO create a component to be populated on click according to section
 
-const FloatingInfoComponent = ({ closeClick, textInfo, image, type }) => {
-  var head1 = "",
-    head2 = "",
-    head3 = "",
-    head4 = "",
-    head5 = "",
-    head6 = "";
-  var desc1 = "",
-    desc2 = "",
-    desc3 = "",
-    desc4 = "",
-    desc5 = "",
-    desc6 = "";
+const FloatingInfoComponent = ({ linkTo,closeClick, textInfo, image, type }) => {
+ 
+  var tempHeaders = [], tempDesc = [], tempLinks = [];
 
-  switch (type) {
-    case "Research":
-      (desc1 = ""),
-        (desc2 = ""),
-        (desc3 = ""),
-        (desc4 = ""),
-        (desc5 = ""),
-        (desc6 = "");
-      (head1 = ""),
-        (head2 = ""),
-        (head3 = ""),
-        (head4 = ""),
-        (head5 = ""),
-        (head6 = "");
-      break;
-    case "Contact":
-      (desc1 = ""),
-        (desc2 = ""),
-        (desc3 = ""),
-        (desc4 = ""),
-        (desc5 = ""),
-        (desc6 = "");
-      (head1 = ""),
-        (head2 = ""),
-        (head3 = ""),
-        (head4 = ""),
-        (head5 = ""),
-        (head6 = "");
 
-      break;
-    case "Visit":
-      (desc1 = ""),
-        (desc2 = ""),
-        (desc3 = ""),
-        (desc4 = ""),
-        (desc5 = ""),
-        (desc6 = "");
-      (head1 = ""),
-        (head2 = ""),
-        (head3 = ""),
-        (head4 = ""),
-        (head5 = ""),
-        (head6 = "");
+  var researchHeaders = [
+    "The Maori Division of Time",
+    "Mātauranga Māori—the ūkaipō of knowledge in New Zealand.",
+    "Mahi Māra: Living by Maramataka Māori in an urban setting",
+    "Maramataka: The Maori Moon Calendar.",
+    "Tangaroa Ara Rau: Tangaroa the Atua of Human Movement.",
+    "Ancient Egyptian Calendars of Lucky and Unlucky Days."
+  ];
 
-      break;
-    default:
-      break;
+  var researchDesc = [
+    "Elsdon B. (2018)",
+    "Hikuroa, D. (2017).",
+    "Hoeta, A. (2020).",
+    "Roberts, M., Weko, F., Clarke, L., (2006)",
+    "Hanara, B., & Jackson, A. (2019).",
+    "Porceddu, S., Jetsu, L., Markkanen, T., & Toivari-Viitala, J. (2008)."
+  ]
+
+  var researchLinks = [
+    "http://nzetc.victoria.ac.nz/tm/scholarly/tei-BesTime-t1-body-d1-d3.html",
+    "Hikuroa, D. (2017). Mātauranga Māori—the ūkaipō of knowledge in New Zealand. In Journal of the Royal Society of New Zealand (Vol. 47, Issue 1, pp. 5–10). https://doi.org/10.1080/03036758.2016.1252407",
+    "https://toitangata.co.nz/2020/04/29/mahi-mara-living-by-maramataka-maori-in-an-urban-setting/",
+    "http://researcharchive.lincoln.ac.nz/handle/10182/155",
+    "http://www.maramatanga.ac.nz/sites/default/files/project-reports/NPM_ngaAkonga_HanaraJackson_18INT01%20%28003%29.pdf",
+    "https://www.mv.helsinki.fi/home/jetsu/papers/egypt1.pdf"
+  ]
+
+  if (type === "Research") {
+    tempHeaders = researchHeaders;
+    tempDesc = researchDesc;
+    tempLinks = researchLinks;
+  } else if (type === "Contact") {
+    tempHeaders = researchHeaders;
+    tempDesc = researchDesc;
+    tempLinks = researchLinks;
+  } else if (type === "Visit") {
+    tempHeaders = researchHeaders;
+    tempDesc = researchDesc;
+    tempLinks = researchLinks;
   }
 
+  
+
+
+   
   return (
     <Container>
       <CloseInfoBtn
@@ -110,40 +97,23 @@ const FloatingInfoComponent = ({ closeClick, textInfo, image, type }) => {
           d="M385 395L235 245"
         />
       </CloseInfoBtn>
-      <Parallax
-        // blur={{ min: -10, max: 10 }}
-        style={{ height: `90vh` }}
-        bgImage={image}
-        strength={300}
-      />
+      <Parallax style={{ height: `80vh` }} bgImage={image} strength={500} />
       <SectionContainer>
-        <SectionBox
-          header={"The Maori Division of Time"}
-          description="Elsdon B. (2018)"
-        />
-        <SectionBox
-          header={"Mātauranga Māori—the ūkaipō of knowledge in New Zealand."}
-          description="Hikuroa, D. (2017)."
-        />
-        <SectionBox
-          header={"Mahi Māra: Living by Maramataka Māori in an urban setting"}
-          description="Hoeta, A. (2020)."
-        />
-        <SectionBox
-          header={"Maramataka: The Maori Moon Calendar."}
-          description="Roberts, M., Weko, F., Clarke, L., (2006)"
-        />
-        <SectionBox
-          header={"Tangaroa Ara Rau: Tangaroa the Atua of Human Movement."}
-          description="Hanara, B., & Jackson, A. (2019)."
-        />
-        <SectionBox
-          header={"Ancient Egyptian Calendars of Lucky and Unlucky Days."}
-          description="Porceddu, S., Jetsu, L., Markkanen, T., & Toivari-Viitala, J. (2008)."
-        />
+        <SectionBox handleClick = {console.log("clicked")}  header={tempHeaders[0]} description={tempDesc[0]} />
+        <SectionBox handleClick = {console.log("clicked")}  header={tempHeaders[1]} description={tempDesc[1]} />
+        <SectionBox handleClick = {console.log("clicked")}  header={tempHeaders[2]} description={tempDesc[2]} />
+        <SectionBox handleClick = {console.log("clicked")}  header={tempHeaders[3]} description={tempDesc[3]} />
+        <SectionBox handleClick = {console.log("clicked")}  header={tempHeaders[4]} description={tempDesc[4]} />
+        <SectionBox handleClick = {console.log("clicked")}  header={tempHeaders[5]} description={tempDesc[5]} />
       </SectionContainer>
     </Container>
   );
 };
+function openInNewTab(link) {
+     
+    window.open(link, '_blank');
+    
+}
+
 
 export default FloatingInfoComponent;
